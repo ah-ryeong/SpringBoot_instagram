e792e81d940d24990aa9dfa535dbe4c2e6ff04b9
unknown <aryeong22@hanail.net>
2020-09-03 10:58:12 +0900

    16. explore(인기사진) 페이지 완료 with 서브쿼리 연습

--- src/main/java/com/cos/instagram/domain/like/LikesRepository.java
+++ src/main/java/com/cos/instagram/domain/like/LikesRepository.java
@@ -3,5 +3,6 @@ package com.cos.instagram.domain.like;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LikesRepository extends JpaRepository<Likes, Integer> {
+	
 
 }
