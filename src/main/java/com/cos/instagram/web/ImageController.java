ea7cf3f73b50bcfc1ab91a9a033712b08b29e829
ah-ryeong <aryeong22@hanmail.net>
2020-09-22 23:29:15 +0900

    25. 프로필 사진 없을 때 버그수정 - 기본 사진 등록 onerror 옵션

--- src/main/java/com/cos/instagram/web/ImageController.java
+++ src/main/java/com/cos/instagram/web/ImageController.java
@@ -49,7 +49,7 @@ public class ImageController {
 		
 		imageService.사진업로드(imageReqDto, loginUser.getId());
 		
-		return "redirect:/";
+		return "redirect:/user/"+loginUser.getId();
 	}
 	
 	@GetMapping("/image/explore")
